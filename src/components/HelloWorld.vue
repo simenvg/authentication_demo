809a97230dc7286753e5ef39359654a77c7fad51
GRINI Simen <simen.grini@soprasteria.com>
2019-11-20 16:46:53 +0100

    Testing api connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,14 +3,30 @@
     <h1>SIMENSIMEN</h1>
     <h1>Dette er pushet nå</h1>
     <h1>Hallo igjen ja</h1>
+    <button class="btn" @click="btnClick">Button</button>
+    <p>{{response}}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return{
+      response: ''
+    }
+  },
+  methods: {
+    btnClick: function(){
+      console.log("HEI");
+      axios
+      .get('https://api-dot-fluent-webbing-257713.appspot.com/mainpage')
+      .then(response => (this.response = response))
+    }
   }
 }
 </script>
@@ -30,5 +46,16 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.btn {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 </style>
